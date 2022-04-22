7b8d61291863e7552e34d907025c7046bd76331d
hfg-nyal-h <nyal.hettmer@hfg.design>
2022-04-22 20:43:04 +0200

    sidebar

--- src/App.vue
+++ src/App.vue
@@ -16,16 +16,69 @@
 </nav>
 
 <!-- End Navbar -->
+<div class="container-fluid" style="postion: absolute;">
 
+    <div class="d-flex flex-column flex-shrink-0 bg-light left" style="width: 4.5rem;">
+    <a href="/" class="d-block p-3 link-dark text-decoration-none" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Icon-only">
+      <svg class="bi" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+      <span class="visually-hidden">Icon-only</span>
+    </a>
+    <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
+      <li class="nav-item">
+        <a href="#" class="nav-link active py-3 border-bottom" aria-current="page" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Home">
+          <svg class="bi" width="24" height="24" role="img" aria-label="Home"><use xlink:href="#home"></use></svg>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link py-3 border-bottom" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
+          <svg class="bi" width="24" height="24" role="img" aria-label="Dashboard"><use xlink:href="#speedometer2"></use></svg>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link py-3 border-bottom" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Orders">
+          <svg class="bi" width="24" height="24" role="img" aria-label="Orders"><use xlink:href="#table"></use></svg>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link py-3 border-bottom" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Products">
+          <svg class="bi" width="24" height="24" role="img" aria-label="Products"><use xlink:href="#grid"></use></svg>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link py-3 border-bottom" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Customers">
+          <svg class="bi" width="24" height="24" role="img" aria-label="Customers"><use xlink:href="#people-circle"></use></svg>
+        </a>
+      </li>
+    </ul>
+    <div class="dropdown border-top">
+      <a href="#" class="d-flex align-items-center justify-content-center p-3 link-dark text-decoration-none dropdown-toggle" id="dropdownUser3" data-bs-toggle="dropdown" aria-expanded="false">
+        <img src="https://github.com/mdo.png" alt="mdo" class="rounded-circle" width="24" height="24">
+      </a>
+      <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser3">
+        <li><a class="dropdown-item" href="#">New project...</a></li>
+        <li><a class="dropdown-item" href="#">Settings</a></li>
+        <li><a class="dropdown-item" href="#">Profile</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item" href="#">Sign out</a></li>
+      </ul>
+    </div>
+    </div>
+    </div>
 
 <!-- Content Container -->
 
+<div class="container" style="
+position: absolute; 
+top:100px; 
+margin-left: auto;
+margin-right: auto;
+left: 100px;
+right: 100px;
+text-align: center;">
 
+  <!-- Content -->
 
-
-<div class="container">
-
-<!-- 1st Row-->
+    <!-- 1st Row-->
   <div class="row">
     <div class="col">
               <p>Canvas:</p>
@@ -57,7 +110,6 @@
         >
       </div>
     </div>
-  </div>
 <!-- 2nd Row -->
   <div class="row">
     <div class="col">
@@ -67,6 +119,11 @@
       2 of 2
     </div>
   </div>
+
+
+  </div>
+
+
 
       <div class="source">
 
@@ -288,5 +345,7 @@ font-size: 8px;
  .output {
   width: 100px;
   height: 200px
-} 
+}
 </style>
+
+
