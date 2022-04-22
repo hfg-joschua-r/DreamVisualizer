d488552557b30eaee27f0c01af8d2229813a7ccc
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2022-04-22 16:58:53 +0200

    remove trash

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
           :eraser="eraser"
           :lineWidth="line"
           :color="color"
+          :lineJoin="miter"
           :background-color="backgroundColor"
           :background-image="backgroundImage"
           :watermark="watermark"
@@ -20,18 +21,9 @@
           :lock="disabled"
           @mousemove="getCoordinate($event)"
         />
-        <p>
-          x-axis: <strong>{{ x }}</strong>, y-axis: <strong>{{ y }}</strong>
-        </p>
+      
         <div class="button-container">
-          <button type="button" @click.prevent="disabled = !disabled">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock" viewBox="0 0 16 16">
-              <path v-if="!disabled" d="M11 1a2 2 0 0 0-2 2v4a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h5V3a3 3 0 0 1 6 0v4a.5.5 0 0 1-1 0V3a2 2 0 0 0-2-2zM3 8a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1H3z"/>
-              <path v-else d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z"/>
-            </svg>
-            <span v-if="!disabled">Unlock</span>
-            <span v-else>Lock</span>
-          </button>
+          
           <button type="button" @click.prevent="$refs.VueCanvasDrawing.undo()">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-counterclockwise" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"/>
@@ -46,13 +38,7 @@
             </svg>
             Redo
           </button>
-          <button type="button" @click.prevent="$refs.VueCanvasDrawing.redraw()">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16">
-              <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"/>
-              <path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"/>
-            </svg>
-            Refresh
-          </button>
+          
           <button type="button" @click.prevent="$refs.VueCanvasDrawing.reset()">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark" viewBox="0 0 16 16">
               <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z"/>
@@ -79,65 +65,22 @@
             <option v-for="n in 25" :key="'option-' + n" :value="n">{{ n }}</option>
           </select>
           <input type="color" v-model="color">
-          <select v-model="strokeType">
-            <option value="dash">
-              Dash
-            </option>
-            <option value="circle">
-              Circle
-            </option>
-            <option value="square">
-              Square
-            </option>
-            <option value="triangle">
-              Triangle
-            </option>
-            <option value="half_triangle">
-              Half Triangle
-            </option>
-          </select>
-          <button type="button" @click.prevent="fillShape = !fillShape">
-            <span v-if="fillShape">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-square-fill" viewBox="0 0 16 16">
-                <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2z"/>
-              </svg>
-              Fill
-            </span>
-            <span v-else>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-square" viewBox="0 0 16 16">
-                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-              </svg>
-              Stroke
-            </span>
-          </button>
+          
         </div>
-        <div class="button-container">
-          <div style="margin-right: 30px;">
-            <p style="margin-bottom: 0">
-              Background Color:
-            </p>
-            <input type="color" v-model="backgroundColor">
-          </div>
-          <div>
-            <p style="margin-bottom: 0">
-              Upload Background Image:
-            </p>
-            <input type="file" @change="setImage($event)">
-          </div>
-          <div>
-            <p style="margin-bottom: 0">
-              Upload Watermark Image:
-            </p>
-            <input type="file" @change="setWatermarkImage($event)">
-          </div>
-        </div>
+       
       </div>
       
       <div class="output">
         <p>Output:</p>
         <img :src="image" style="border: solid 1px #000000">
+        <p>
+          {{image}}
+        </p>
       </div>
+      <p>
+      </p>
     </div>
+
 </template>
 
 <script>
